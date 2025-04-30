035ee0b6fe7aad6e4d050f91157aca5c798010a9
vasavjain1267 <149325200+vasavjain1267@users.noreply.github.com>
2025-04-30 15:25:58 +0530

    Update auction.ts

--- frontend/app/lib/auction.ts
+++ frontend/app/lib/auction.ts
@@ -1,6 +1,6 @@
 
 import AuctionSystem from "../../../backend/build/contracts/AuctionSystem.json"
 
-export const AUCTION_ADDRESS = "0x15e51E54a0Cc4FF5018662cF89C9fb6C1502FA7F"
+export const AUCTION_ADDRESS = "your_deployed_contract_address_here"
 
 export const AUCTION_ABI=AuctionSystem.abi
